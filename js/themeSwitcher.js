24158a50566d64d59a2ec2453063012da20cb31f
LuPro <luis.immanuel@gmx.at>
2021-10-08 18:17:35 +0200

    fix event subscription, still needs some love but works as a proof of concept

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -2,6 +2,7 @@ var __pathOffset;
 var __themes;
 var __themePrimary;
 var __themeSecondary;
+var __subscribers = [];
 
 function initThemes(pathOffset, themes)
 {
@@ -25,21 +26,41 @@ function toggleTheme()
     let themeSwitcherButton = $(document).find("button#themeSwitcher");
     let newTheme;
     let newIcon;
+    let newType;
     if (theme.getAttribute('href') == `${__pathOffset}themes/${__themes[0]['theme']}.css`)
     {
         newTheme = __themes[1]['theme'];
+        newType = __themes[1]['type'];
         newIcon = __themes[0]['icon'];
     } 
     else if (theme.getAttribute('href') == `${__pathOffset}themes/${__themes[1]['theme']}.css`)
     {
         newTheme = __themes[0]['theme'];
+        newType = __themes[0]['type'];
         newIcon = __themes[1]['icon'];
     }
 
     theme.setAttribute('href', `${__pathOffset}themes/${newTheme}.css`);
     themeSwitcherButton.html(`<i class="bi bi-${newIcon}"></i>`);
 
-    const event = new CustomEvent('themeToggle', {detail: {theme: newTheme}});
-    dispatchEvent(event);
+    const event = new CustomEvent('themeToggle', {detail: {theme: newTheme, type: newType}});
 
+    __subscribers.forEach((subscriber) => {
+        subscriber["elem"].dispatchEvent(event);
+    });
 }
+
+function subscribe(elem, listener)
+{
+    //this implementation doesn't allow the same element to subscribe with two listeners - would I ever want that? if so, it could be handled by a "wrapper listener" that pushes the event to other "sub listeners"
+    if (!__subscribers.includes(elem))
+    {
+        elem.addEventListener("themeToggle", listener);
+        __subscribers.push(elem);
+        return true;
+    }
+    return false;
+}
+
+
+
