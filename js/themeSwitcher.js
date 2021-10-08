df8fdbf391fdd469619d621dfa6a09307fb239d3
LuPro <luis.immanuel@gmx.at>
2021-10-08 16:56:57 +0200

    fix issue with path offset

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -7,7 +7,7 @@ function initThemes(pathOffset, themes)
 {
     __pathOffset = pathOffset;
     __themes = themes;
-    theme.setAttribute('href', `${pathOffset}themes/${__themes[0]['theme']}.css`);
+    theme.setAttribute('href', `${__pathOffset}themes/${__themes[0]['theme']}.css`);
 }
 
 //theme is a dict of an icon (string) and the name of the theme file (without .css)
@@ -25,18 +25,18 @@ function toggleTheme()
     let themeSwitcherButton = $(document).find("button#themeSwitcher");
     let newTheme;
     let newIcon;
-    if (theme.getAttribute('href') == `${pathOffset}themes/${__themes[0]['theme']}.css`)
+    if (theme.getAttribute('href') == `${__pathOffset}themes/${__themes[0]['theme']}.css`)
     {
         newTheme = __themes[1]['theme'];
         newIcon = __theme[0]['icon'];
     } 
-    else if (theme.getAttribute('href') == `${pathOffset}themes/${__themes[1]['theme']}.css`)
+    else if (theme.getAttribute('href') == `${__pathOffset}themes/${__themes[1]['theme']}.css`)
     {
         newTheme = __themes[0]['theme'];
         newIcon = __themes[1]['icon'];
     }
 
-    theme.setAttribute('href', `${pathOffset}themes/${newTheme}.css`);
+    theme.setAttribute('href', `${__pathOffset}themes/${newTheme}.css`);
     themeSwitcherButton.html(`<i class="bi bi-${newIcon}"></i>`);
 
     const event = new CustomEvent('themeToggle', {detail: {theme: newTheme}});
